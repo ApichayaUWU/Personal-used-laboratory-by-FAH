3e822e347b8f029c8f889565ab30d5b055d497d5
apichaya <apichaya_ink@cmu.ac.th>
2022-12-13 20:20:29 +0700

    add calcutor complex formular

---
+++ lab_complexfomular.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+double term1(double t)
+{
+    t = sin((2*M_PI)/(3*pow(t,4)+1));
+    return t;
+}
+
+double term2(double t)
+{
+    t = (pow(2,(2*t)+1))*(sqrt(pow(t,2)+1));
+    return t;
+}
+
+double complexFormula(double x)
+{
+    double result;
+    result = term1(x)+term2(x);
+    return result;
+
+}
+
+int main ()
+{
+    cout << "Result = " << complexFormula(1);
+    return 0;
+}
