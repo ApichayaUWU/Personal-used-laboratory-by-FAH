4c3254439c93b3ae9fe315ab6445b4fcad597126
apichaya <apichaya_ink@cmu.ac.th>
2022-12-13 18:12:07 +0700

    another series 1/x+..1/69

--- lab_sumofseries.cpp
+++ lab_sumofseries.cpp
@@ -2,12 +2,11 @@
 #include <cmath>
 using namespace std;
 
-int main()
-{
-    float x = 5, sum = 0;
-    while(x <= 225){
-        sum = sum + x;
-        x = x + 10;
+int main(){
+    float x = 2, sum = 0;
+    while(x <= 69){
+        sum += 1/x;
+        x++;
     }
     cout << sum;    
     return 0;
