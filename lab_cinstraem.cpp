5a475a6c18ab59136f711b24cfb7b02ad14dd89e
apichaya <apichaya_ink@cmu.ac.th>
2022-12-20 12:04:56 +0700

    add test input stream

---
+++ lab_cinstraem.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+    cout << " Input number => ";
+    cin >> a;
+    cin >>b ;
+    cin >> c;
+
+    cout << a << "  ---   " << b << "  ---  " << c;
+
+}
