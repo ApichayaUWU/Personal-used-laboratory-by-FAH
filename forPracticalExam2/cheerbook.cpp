301563e6786af036c437d9d21f698f060937924e
apichaya <apichaya_ink@cmu.ac.th>
2023-02-03 00:45:59 +0700

    complete

---
+++ forPracticalExam2/cheerbook.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+using namespace std;
+
+int main (){
+	ifstream source;
+	ofstream dest;
+	source.open("cheerbook.txt");
+	dest.open("cheerbook_copy.txt");
+	
+    string text;
+    dest << "-------------------- BOOM ---------------------\n";
+    while(getline(source,text))
+    {
+        dest << text << endl;
+    }
+    dest << "-------------------- HA!! ---------------------";
+
+
+    source.close();
+    dest.close();
+	return 0;
+}
